be9034b4226cf24583aef1c0e183b55a5a8b9975
JureLokovsek <jurext@hotmail.com>
2020-02-02 15:48:24 +0100

    Todo.

--- app/src/main/java/com/learn/music_player_app/MainActivity.java
+++ app/src/main/java/com/learn/music_player_app/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // TODO: https://www.youtube.com/watch?v=A2uR94W5Itg&feature=youtu.be&fbclid=IwAR19oznoYl49sME_bjb3iFddX4YmWObbA6Kwf3h_TWtH9ltmoEpn8IJosds
     }
 }
